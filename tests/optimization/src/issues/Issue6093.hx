c9f1a2a6ab9a861cc5bdbeaa6b55e3c6a92e90a4
Mario <mario.e.carbajal@gmail.com>
2017-03-16 20:15:37 -0300

    Add optimization test for issue #6093

---
+++ tests/optimization/src/issues/Issue6093.hx
@@ -0,0 +1,23 @@
+package issues;
+
+private class A {
+	public var x : Int = 1;
+	public inline function new () {};
+}
+
+private class B extends A {
+	public var y : Int = 2;
+	public inline function new () { super(); };
+}
+
+class Issue6093 {
+	@:js('
+		var a_y;
+		var a_x;
+		a_y = 2;
+		a_x = 1;
+	')
+	static public function main() {
+		var a = new B();
+	}
+}
