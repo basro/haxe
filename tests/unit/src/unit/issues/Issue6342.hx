a1eb9d15fb691eaee64816c3f02d4df44e11996a
Mario <mario.e.carbajal@gmail.com>
2017-06-07 17:00:37 -0300

    Test for issue #6342

---
+++ tests/unit/src/unit/issues/Issue6342.hx
@@ -0,0 +1,29 @@
+package unit.issues;
+
+class Issue6342 extends unit.Test {
+	function test() {
+	{
+		var t = new A(1);
+		var tmp1 = t.foo().x;
+		eq(1, tmp1);
+		
+		var tmp2 = ({
+			new A(2);
+		}).x;
+		eq(2, tmp2);
+	}
+}
+
+private class A
+{
+	public var x:Float;
+	
+	public inline function new(x) {
+		this.x = x;
+	}
+	
+	public inline function foo() {
+		var tmp = x;
+		return new A(x);
+	}
+}
