2256a8b216ace5e4612fa835047772a98c18d2d6
Mario Carbajal <mario.e.carbajal@gmail.com>
2020-07-01 15:53:55 -0300

    Adapt inline IntMap iterator from #8806

--- std/js/_std/haxe/ds/IntMap.hx
+++ std/js/_std/haxe/ds/IntMap.hx
@@ -22,6 +22,28 @@
 
 package haxe.ds;
 
+private class IntMapIterator<T> {
+	var map:IntMap<T>;
+	var keys:Array<Int>;
+	var index:Int;
+	var count:Int;
+
+	public inline function new(map:IntMap<T>, keys:Array<Int>) {
+		this.map = map;
+		this.keys = keys;
+		this.index = 0;
+		this.count = keys.length;
+	}
+
+	public inline function hasNext() {
+		return index < count;
+	}
+
+	public inline function next() {
+		return map.get(keys[index++]);
+	}
+}
+
 @:coreApi class IntMap<T> implements haxe.Constraints.IMap<Int, T> {
 	private var h:Dynamic;
 
@@ -49,23 +71,17 @@ package haxe.ds;
 	}
 
 	public function keys():Iterator<Int> {
-		var a = [];
-		js.Syntax.code("for( var key in {0} ) if({0}.hasOwnProperty(key)) {1}.push(key | 0)", h, a);
-		return a.iterator();
+		return arrayKeys().iterator();
 	}
 
-	public function iterator():Iterator<T> {
-		return untyped {
-			ref: h,
-			it: keys(),
-			hasNext: function() {
-				return __this__.it.hasNext();
-			},
-			next: function() {
-				var i = __this__.it.next();
-				return __this__.ref[i];
-			}
-		};
+	inline function arrayKeys():Array<Int> {
+		var a = [];
+		js.Syntax.code("for( var key in {0} ) if({0}.hasOwnProperty(key)) {1}.push(key | 0)", h, a);
+		return a;
+	}
+
+	public inline function iterator():Iterator<T> {
+		return new IntMapIterator(this, arrayKeys());
 	}
 
 	@:runtime public inline function keyValueIterator():KeyValueIterator<Int, T> {
